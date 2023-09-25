d5435abb9294f8926db3f44c3816c3028df69e95
benjaAguilar <benjaaguilar02@gmail.com>
2023-09-25 08:30:52 -0300

    created a function playRound
    allows you to play a round of rock paper scissors

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
+function playRound(){
+    let computer = getComputerSelection();
+    let player = getPlayerSelection();
+    let result = "you choose: " + player + "\n computer choose: " + computer;
+    console.log(result);
+
+    if(player === computer){
+        return "its a tie!"
+
+    } else if(computer === "rock" && player === "scissors"){
+        return "You lose!"
+
+    } else if(computer === "paper" && player === "rock"){
+        return "You lose!"
+
+    } else if(computer === "scissors" && player === "paper"){
+        return "You lose!"
+
+    } else{
+        return "You won!"
+
+    }
+}
+
+function game(){
+    for(var i = 1; i <= 5; i++){
+
+    }
+}
 
 function getComputerSelection(){
     let randomNumber = Math.floor(Math.random() * 3) + 1;
