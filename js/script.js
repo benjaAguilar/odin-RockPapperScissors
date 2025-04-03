16d8039497d64882cbe0bc11e25f2f8ae2840d4f
ColorCode <benjaaguilar02@gmail.com>
2025-04-03 10:51:04 -0300

    feat: add js compile file

--- js/script.js
+++ js/script.js
@@ -1,143 +1,132 @@
-let pcHealth = 5;
-let playerHealth = 5;
-
-let rockBtn = document.querySelector('#rock-btn');
-let paperBtn = document.querySelector('#paper-btn');    
-let scissorsBtn = document.querySelector('#scissors-btn');
-let resetBtn = document.querySelector('#reset');
-
-rockBtn.addEventListener("click", () => { playRound("rock") });
-paperBtn.addEventListener("click", () => { playRound("paper") });
-scissorsBtn.addEventListener("click", () => { playRound("scissors") });
-resetBtn.addEventListener("click", () => {location.reload()});
-
-resetBtn.style.display = "none";
-
-function playRound(player){
-    let pc = getPcSelection();
-
-    showSelection(player, pc);
-    let resultSection = printResults(player, pc);
-
-    if(player === pc){
+var pcHealth = 5;
+var playerHealth = 5;
+var rockBtn = document.querySelector("#rock-btn");
+var paperBtn = document.querySelector("#paper-btn");
+var scissorsBtn = document.querySelector("#scissors-btn");
+var resetBtn = document.querySelector("#reset");
+rockBtn === null || rockBtn === void 0 ? void 0 : rockBtn.addEventListener("click", function () {
+    playRound("rock");
+});
+paperBtn === null || paperBtn === void 0 ? void 0 : paperBtn.addEventListener("click", function () {
+    playRound("paper");
+});
+scissorsBtn === null || scissorsBtn === void 0 ? void 0 : scissorsBtn.addEventListener("click", function () {
+    playRound("scissors");
+});
+resetBtn === null || resetBtn === void 0 ? void 0 : resetBtn.addEventListener("click", function () {
+    location.reload();
+});
+if (resetBtn)
+    resetBtn.style.display = "none";
+function playRound(playerSelection) {
+    var pcSelection = getPcSelection();
+    showSelection(playerSelection, pcSelection);
+    var resultSection = printResults(playerSelection, pcSelection);
+    if (!resultSection) {
+        alert("Oops there was an unknown problem!");
+        location.reload();
+        return;
+    }
+    if (playerSelection === pcSelection) {
         resultSection.textContent = "its a tie!";
-
-    } else if(pc === "rock" && player === "scissors"){
+    }
+    else if (pcSelection === "rock" && playerSelection === "scissors") {
         healthSystem("player");
         resultSection.textContent = "You lose!";
-
-    } else if(pc === "paper" && player === "rock"){
+    }
+    else if (pcSelection === "paper" && playerSelection === "rock") {
         healthSystem("player");
         resultSection.textContent = "You lose!";
-
-    } else if(pc === "scissors" && player === "paper"){
+    }
+    else if (pcSelection === "scissors" && playerSelection === "paper") {
         healthSystem("player");
         resultSection.textContent = "You lose!";
-
-    } else{
+    }
+    else {
         healthSystem("pc");
         resultSection.textContent = "You won!";
-
     }
 }
-
-function healthSystem(loseHealth){
-    if(loseHealth === "player"){
-        let heart = document.querySelector('.player-lives img');
+function healthSystem(loseHealth) {
+    if (loseHealth === "player") {
+        var heart = document.querySelector(".player-lives img");
         heart.remove();
         playerHealth--;
-
-    } else if(loseHealth === "pc"){
-        let heart = document.querySelector('.pc-lives img');
+    }
+    else if (loseHealth === "pc") {
+        var heart = document.querySelector(".pc-lives img");
         heart.remove();
         pcHealth--;
-
     }
-
-    if(playerHealth === 0 || pcHealth === 0){
+    if (playerHealth === 0 || pcHealth === 0) {
         endGame();
     }
 }
-
-function getPcSelection(){
-    let randomNumber = Math.floor(Math.random() * 3) + 1;
-    let computerSelection;
-
-    if(randomNumber === 1){
+function getPcSelection() {
+    var randomNumber = Math.floor(Math.random() * 3) + 1;
+    var computerSelection;
+    if (randomNumber === 1) {
         computerSelection = "rock";
-
-    } else if(randomNumber === 2){
-        computerSelection = "paper";
-    
-    } else{
-        computerSelection = "scissors";
-
     }
-
+    else if (randomNumber === 2) {
+        computerSelection = "paper";
+    }
+    else {
+        computerSelection = "scissors";
+    }
     return computerSelection;
 }
-
-function showSelection(player, pc){
-    let playerImg = document.querySelector('#player-img');
-    let pcImg = document.querySelector('#pc-img');
-
-    switch(player){
+function showSelection(player, pc) {
+    var playerImg = document.querySelector("#player-img");
+    var pcImg = document.querySelector("#pc-img");
+    switch (player) {
         case "rock":
             playerImg.src = "imgs/rock.svg";
-        break;
+            break;
         case "paper":
             playerImg.src = "imgs/paper.svg";
-        break;
+            break;
         case "scissors":
             playerImg.src = "imgs/scissors.svg";
-        break;
+            break;
     }
-    
-    switch(pc){
+    switch (pc) {
         case "rock":
             pcImg.src = "imgs/rock.svg";
-        break;
+            break;
         case "paper":
             pcImg.src = "imgs/paper.svg";
-        break;
+            break;
         case "scissors":
             pcImg.src = "imgs/scissors.svg";
-        break;
+            break;
     }
-
 }
-
-function printResults(player, computer){
-
-    let resultSection = document.querySelector('.results h2');
-
-    let result = "you choose: " + player + " computer choose: " + computer;
-
-    let para = document.querySelector('#para');
+function printResults(player, computer) {
+    var resultSection = document.querySelector(".results h2");
+    var result = "you choose: " + player + " computer choose: " + computer;
+    var para = document.querySelector("#para");
     para.textContent = result;
-
-    return resultSection
+    return resultSection;
 }
-
-function endGame(){
-    let finalScore;
-
-    if(playerHealth > pcHealth){
+function endGame() {
+    var finalScore;
+    if (playerHealth > pcHealth) {
         finalScore = "YOU ARE VICTORIUS!";
-
-    } else{
-        finalScore = "DEFEATED!";
-
     }
-
-    let parentNode = document.querySelector('.main h1');
-    let endResult = document.createElement('h2');
+    else {
+        finalScore = "DEFEATED!";
+    }
+    var parentNode = document.querySelector(".main h1");
+    var endResult = document.createElement("h2");
     endResult.textContent = finalScore;
     parentNode.after(endResult);
-
-    resetBtn.style.display = "block";
-    rockBtn.disabled = true;
-    paperBtn.disabled = true;
-    scissorsBtn.disabled = true;
-    
+    if (resetBtn)
+        resetBtn.style.display = "block";
+    if (rockBtn)
+        rockBtn.disabled = true;
+    if (paperBtn)
+        paperBtn.disabled = true;
+    if (scissorsBtn)
+        scissorsBtn.disabled = true;
 }
